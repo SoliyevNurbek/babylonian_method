50c9042582262e1aa30f0a6bcb4e27ae4beb51a9
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-09-25 16:48:59 +0500

    Babilon metod

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def main(S, d):
-    '''create a babylonian function.
-    
-    Args:
-        S (int): number
-        d (int): numnber
-        
-    Returns:
-        float: result
-    '''
-    return
+def main(S,d):
+    a=(S-d**2)/(2*d)
+    b=a+d
+    x=b-a*a/(2*b)
+    return(x)
+print(main(26,5))
